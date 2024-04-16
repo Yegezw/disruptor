2817ff029ea5a91352aa1fbeef41ae06101d6286
ld <zzw422339@163.com>
2024-04-17 01:22:53 +0800

    添加工具类

---
+++ src/main/java/com/zzw/util/Util.java
@@ -0,0 +1,44 @@
+package com.zzw.util;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class Util
+{
+
+    private Util()
+    {
+    }
+
+    public static void sleep(long millis)
+    {
+        try
+        {
+            Thread.sleep(millis);
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final Unsafe UNSAFE;
+
+    static
+    {
+        try
+        {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            UNSAFE = (Unsafe) theUnsafe.get(null);
+        } catch (Exception e)
+        {
+            throw new RuntimeException("Loading Unsafe failed");
+        }
+    }
+
+    public static Unsafe getUnsafe()
+    {
+        return UNSAFE;
+    }
+}
